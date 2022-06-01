d8bc265f8c50af6a2fe17d75cffabfb0c4d8496e
Brian McAuliffe <brian.mcauliffe2@dell.com>
2022-06-01 18:07:44 +0100

    header and footer

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  months = ['may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec', 'jan', 'feb', 'mar', 'apr']
+  expenses: any[]= [];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.expenses = [
+      {
+        title: "mortgage",
+        items: [1100,1100,1100,1100,1100,1100,1100,1100,1100,1100,1100,1100]
+      },
+      {
+        title: "gas",
+        items: [175,0,175,0,175,0,175,0,175,0,175,0]
+      },
+      {
+        title: "electricity",
+        items: [400,0,400,0,400,0,400,0,400,0,400,0]
+      }
+    ]
   }
 
 }
